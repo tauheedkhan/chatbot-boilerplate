0186fbcfe611bffbe8a38bb49ab366c3f555a5b5
tauheedkhan <tauheedkhan@outlook.com>
2018-10-16 00:30:35 +0100

    minor config changes

--- server/api/services/discourse/discourse.handler.js
+++ server/api/services/discourse/discourse.handler.js
@@ -1,5 +1,5 @@
 export default function discourseHandler(r) {
 
-  // Ths is the place where you can intersept the response for the whatson server and do the manupluation for the UI, for example setting the context.
+  // Ths is the place where you can intersept the response for the watson server and do the manupluation for the UI, for example setting the context.
 return r;
 }
